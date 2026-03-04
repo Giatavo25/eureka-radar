abd2ee8e4647ee1ee4b4e296d0e2ca74df9cd93c
Giatavo25 <gv9545513@gmail.com>
2026-03-04 15:47:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-import streamlit
+import streamlit as st
+
+st.set_page_config(page_title="RADAR EUREKA")
+st.title("🎯 Radar Eureka")
+st.header("¡Hola Gustavo! Ya estamos en vivo.")
+
+if st.button('🚀 PROBAR ESCÁNER'):
+    st.balloons()
+    st.success("El sistema funciona. ¡Dime si ves los globos!")
